b5fcc1f61c1849e66e92e5fc9447c87311af0ea5
Ethan Bruning <ethan@modsterinc.com>
2010-09-03 22:08:00 -0700

    Added shell script to link files and directories

---
+++ update_links.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/bash
+
+##### Create the sym links for the config files
+DOTFILES_DIR=~/.dotfiles
+config_files=( bash_profile vimrc gitconfig gitignore )
+config_directories=( filezilla vim )
+
+symlink_files() {
+	for name in ${config_files[@]}
+		do
+			ln -s $DOTFILES_DIR/$name ~/.dotfiles/tmp/.$name
+		    echo "\tlinking $name...."
+		done
+}
+
+symlink_directories() {
+	for name in ${config_directories[@]}
+		do 
+			ln -s $DOTFILES_DIR/$name ~/.dotfiles/tmp/.$name
+			echo "\tlinking $name...."
+		done
+}
+
+symlink_all() {
+	symlink_files
+	symlink_directories
+}
+
+unlink_all() {
+	for name in ${config_files[@]}
+		do
+			unlink ~/.dotfiles/tmp/.$name
+			echo "\tunlinking $name"
+		done
+	
+	for name in ${config_directories[@]}
+		do
+			unlink ~/.dotfiles/tmp/.$name
+			echo "\tunlinking $name"
+		done
+}
+
+usage(){
+	echo "Usage:"
+	echo "\tunlink -> unlinks all files and directories"
+	echo "\tlink -> links all files and directories"	
+}
+
+if [ $# -ne 1 ] ; then
+	usage
+	exit
+fi
+
+if [ $1 = "link" ] ; then
+	echo "Linking files and directories:"
+	symlink_all
+elif [ $1 = "unlink" ]; then
+	echo "Unlinking files and directories:"
+	unlink_all
+else
+	echo "Invalid option"
+	usage
+fi
